d3f52433eae8d6feb8c1c43afbcb845e66d9933f
Michael D. Hoyle <hoylemd@gmail.com>
2015-03-10 08:35:54 -0400

    some good validation

--- column_1/rand_generator/rand_gen.py
+++ column_1/rand_generator/rand_gen.py
@@ -4,11 +4,26 @@ import sys
 def gen_random(count, maximum):
   return random.sample(range(maximum), count)
 
+invalid_params_string="  passed: count=%s, maximum=%s";
+
 if len(sys.argv) < 3:
   print "Usage: python rand_sample.py $count $maximum"
 else:
-  count=int(sys.argv[1])
-  maximum=int(sys.argv[2])
+  count=sys.argv[1]
+  maximum=sys.argv[2]
+  ok=False
+  try:
+    count=int(count)
+    maximum=int(maximum)
+    ok=True
+  except ValueError:
+    print "Please pass numbers for count and maximum"
+    print invalid_params_string % (count, maximum)
 
-  for num in gen_random(count, maximum):
-    print num
+  if ok:
+    try:
+      for num in gen_random(count, maximum):
+        print num
+    except ValueError:
+      print "Maximum must be greater than count"
+      print invalid_params_string % (count, maximum)
