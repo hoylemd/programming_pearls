e1677a5be9bf8df41c5d25e7be9fa99bc2f15cbd
Michael D. Hoyle <hoylemd@gmail.com>
2015-03-10 08:23:35 -0400

    functionalize and make nicely callable

--- column_1/rand_generator/rand_gen.py
+++ column_1/rand_generator/rand_gen.py
@@ -1,7 +1,14 @@
 import random
+import sys
 
 def gen_random(count, maximum):
   return random.sample(range(maximum), count)
 
-for num in gen_random(5, 100):
-  print num
+if len(sys.argv) < 3:
+  print "Usage: python rand_sample.py $count $maximum"
+else:
+  count=int(sys.argv[1])
+  maximum=int(sys.argv[2])
+
+  for num in gen_random(count, maximum):
+    print num
