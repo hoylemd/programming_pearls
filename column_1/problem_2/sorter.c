39af223a3eea44b6f05247b1ec360a4a7ffe857b
Michael D. Hoyle <hoylemd@gmail.com>
2015-03-10 08:50:44 -0400

    add read/write ramework and test file

--- column_1/problem_2/sorter.c
+++ column_1/problem_2/sorter.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void) {
-  printf("hello, world!\n");
+  int a[100], n = 0, i = 0;
+
+  while(scanf("%d", &a[n]) != EOF) {
+    n += 1;
+  }
+
+  // do the sort here
+
+  for (i = 0; i < n; i += 1) {
+    printf("%d\n", a[i]);
+  }
+
   return 0;
 }
