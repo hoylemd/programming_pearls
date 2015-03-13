610b23bc680216ee9bc00eda7c1c0ac4242134e1
Michael D. Hoyle <hoylemd@gmail.com>
2015-03-13 19:33:17 -0400

    implemented

--- column_1/rand_generator/rando.c
+++ column_1/rand_generator/rando.c
@@ -1,7 +1,29 @@
 #include "rando.h"
+int randint(int min, int max) {
+  int r = 0, range = max - min;
 
+  r = rand() % range;
+
+  return r + min;
+}
 
 int main(void) {
-  printf("Hello, world!\n");
+  int numbers[MAX], i = 0, target = 0, temp = 0;
+
+  // seeds
+  srand(time(NULL));
+  for (i = 0; i < MAX; i += 1) {
+    numbers[i] = i;
+  }
+
+  // randomize
+  for (i = 0; i < NUMBER; i += 1) {
+    target = randint(i, MAX);
+    temp = numbers[target];
+    numbers[target] = numbers[i];
+    numbers[i] = temp;
+    printf("%d\n", temp);
+  }
+
   return 0;
 }
--- column_1/rand_generator/rando.h
+++ column_1/rand_generator/rando.h
@@ -1,2 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#define MAX	200
+#define NUMBER	10
